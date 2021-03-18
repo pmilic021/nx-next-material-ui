0ca92ce47a43522330dfd62aa6737224e51d0d98
Petar Milic <pmilic@laplacian.hr>
2021-03-18 19:42:35 +0100

    added material ui

--- apps/api/pages/_app.tsx
+++ apps/api/pages/_app.tsx
@@ -3,8 +3,17 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
 import './styles.css';
+import { Box } from '@material-ui/core';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -15,9 +24,13 @@ function CustomApp({ Component, pageProps }: AppProps) {
           <NxLogo width="75" height="50" />
           <h1>Welcome to api!</h1>
         </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
+        <h1 style={{color: "red"}}>TEST STRING</h1>
+        <Box
+          css={{ color: "red" }}
+          component='h1'>Test string 2</Box>
+        {/*<main>*/}
+        {/*  <Component {...pageProps} />*/}
+        {/*</main>*/}
       </div>
     </>
   );
